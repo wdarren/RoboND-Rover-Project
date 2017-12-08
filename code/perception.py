b047adf359f8eaefee8a3db7246244cf1d2bee8f
Darren Wang <wdarren@gmail.com>
2017-12-08 19:42:51 +0800

    Add mask to perception_transform

--- code/perception.py
+++ code/perception.py
@@ -73,8 +73,8 @@ def pix_to_world(xpix, ypix, xpos, ypos, yaw, world_size, scale):
 def perspect_transform(img, src, dst):
            
     M = cv2.getPerspectiveTransform(src, dst)
-    warped = cv2.warpPerspective(img, M, (img.shape[1], img.shape[0]))# keep same size as input image
-    
+    warped = cv2.warpPerspective(img, M, (img.shape[1]), img.shape[0]))# keep same size as input image
+    mask = cv2.warpPerspective(np.ones_like(img[:,:,0], M, (img.shape[1], img.shape[0]))
     return warped
 
 
